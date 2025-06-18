55442f846b3d8040ca4c7acf6bb71820b73ccf3a
hakmoune <mehdi.hakmoune@gmail.com>
2025-06-18 03:03:52 +0200

    Start learning docker

---
+++ node-app/src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my Node.js app! This is the home page !!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+}); //10min
