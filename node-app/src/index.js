f02e30cbbfcc3b0970f6d26894f9fb4886b19a66
hakmoune <mehdi.hakmoune@gmail.com>
2025-06-20 02:30:10 +0200

    docker-compose

--- node-app/src/index.js
+++ node-app/src/index.js
@@ -5,9 +5,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Welcome to my Node.js app! This is the home page !!");
+  res.send("Welcome to my Node.js app! This is the home page !");
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
-}); //10min
+});
