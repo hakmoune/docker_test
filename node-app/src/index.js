7b019d9f757081440361d4ad35c4cc476bd73126
hakmoune <mehdi.hakmoune@gmail.com>
2025-06-21 15:24:11 +0200

    env variable PART 2

--- node-app/src/index.js
+++ node-app/src/index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Welcome to my Node.js app! This is the home page !");
+  res.send("Welcome to my Node.js app! This is the home page !!!");
 });
 
 app.listen(PORT, () => {
