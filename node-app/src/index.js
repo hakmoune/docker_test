f9f34caa4c63cade55bc56f9b4cb782c5d30b873
hakmoune <mehdi.hakmoune@gmail.com>
2025-06-23 18:57:50 +0200

    MongoDB container

--- node-app/src/index.js
+++ node-app/src/index.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
+// connect db
+const DB_USER = "root";
+const DB_PASSWORD = "example";
+const DB_PORT = 27017;
+const DB_HOST = "mongo"; // tu peux passer l'IP de ton conteneur MongoDB ici ou DNS name if using Docker Compose 'service name', 'container name', 'ID'.
+
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+mongoose
+  .connect(URI)
+  .then(() => {
+    console.log("Connected to MongoDB successfully!");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB:", err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Welcome to my Node.js app! This is the home page !!!");
