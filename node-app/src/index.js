1d04fcb6457b075c058fe126e35ce65344c1cae1
El MAHDI HAKMOUNE <heymehdi@MacBook-Pro-de-El.local>
2025-07-06 04:04:52 +0200

    Test github on new laptop

--- node-app/src/index.js
+++ node-app/src/index.js
@@ -14,7 +14,7 @@ const redisClient = redis.createClient({
 });
 redisClient.on("error", (err) => console.log("Redis Client Error", err));
 redisClient.on("connect", () =>
-  console.log("Connected to Redis successfully!")
+  console.log("Connected to Redis successfully !")
 );
 
 redisClient.connect();
